cbf7f0c963f67c96d80d1cfbc4b07133e9e8a14e
Barashek <38440552+Barashek@users.noreply.github.com>
2019-07-09 10:48:25 +0300

    3

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -11,11 +11,9 @@ class GalleryController extends Controller
 
     public function index()
     {
-        $artists = Artist::all();
+        // $artists = Artist::all();
         $artist = Artist::where('id', '=', 1)->first();
-        $paintings = Painting::where('artist_id', '=', '1')->get();
         return view('gallery', [
-            'paintings' => $paintings,
             'artists' => $artists,
             'artist' => $artist
         ]);
--- resources/views/expositions.blade.php
+++ resources/views/expositions.blade.php
@@ -7,10 +7,17 @@
 @section("content")
 
 <div class="row">
-    <div class="col-lg-9">
+    <div class="col-lg-8">
         @foreach($expositions as $exp)
             <p class="exposition">{{ $exp->year }} — {{ $exp->place }}</p>
         @endforeach
     </div>
+    <div class="col-lg-4 exposition-img">
+        <img src="img/Изображение 2.jpg">
+        <img src="img/Изображение 1.jpg">
+        <img src="img/Изображение 3.jpg">        
+    </div>        
 </div>
+
+
 @endsection
