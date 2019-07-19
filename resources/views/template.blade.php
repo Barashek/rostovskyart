22bb501f70fcde3f682804fd484a89e5f5b72348
Barashek <uictoriam1367@gmail.com>
2019-07-19 16:51:52 +0300

    ничего особенного

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,13 +1,13 @@
 @extends('navbar')
 @section('container')
 
-    <div class="row d-flex justify-content-around">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10" id="content">
-            @yield("content")
-        </div>
-        <div class="col-lg-1"></div>
+<div class="row d-flex justify-content-around">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-10" id="content">
+        @yield("content")
     </div>
-    <div class="row" id="footer"></div>
+    <div class="col-lg-1"></div>
+</div>
+<div class="row" id="footer"></div>
 
 @endsection
