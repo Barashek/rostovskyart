66d9186eb1483fcf71e46f9f0bddd21538ac148f
Barashek <uictoriam1367@gmail.com>
2019-08-16 22:29:03 +0300

    summary

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,11 @@
 window.onload = function() {
+    backGround();
+}
+window.onresize = function() {
+    backGround();
+}
+
+function backGround() {
     if (window.matchMedia("(orientation: portrait)").matches) {
         $("#bg").html("<img src='img/bg-portrait.jpg'>");
     } else {
